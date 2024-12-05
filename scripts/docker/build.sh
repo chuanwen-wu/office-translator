0db460e52e82ad3af430c4cbab120831686871c3
Kenneth Wu <wcw84@qq.com>
2024-12-05 22:58:47 +0700

    use cache when buildx

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -2,8 +2,8 @@
 
 if [[ $1 == 'all' ]]; then
     echo "build mac and linux release both" 
-    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator:latest -f Dockerfile-controller ../../ --push
-    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator-web-app:latest -f Dockerfile-web-app ../../web-app/ --push
+    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator:latest -f Dockerfile-controller ../../ --push --cache-from sebalaxi/office-translator:latest
+    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator-web-app:latest -f Dockerfile-web-app ../../web-app/ --push -cache-from sebalaxi/office-translator-web-app:latest
 else
     echo "build only mac release" 
     docker build -t sebalaxi/office-translator:latest -f Dockerfile-controller ../../
