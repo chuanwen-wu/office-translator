4d5574675bc872fd1456f66852abba9cc7edc7eb
Kenneth Wu <wcw84@qq.com>
2024-12-06 14:11:30 +0700

    update

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -2,8 +2,8 @@
 
 if [[ $1 == 'all' ]]; then
     echo "build mac and linux release both" 
-    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator:latest -f Dockerfile-controller ../../ --push --cache-from sebalaxi/office-translator:latest
-    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator-web-app:latest -f Dockerfile-web-app ../../web-app/ --push -cache-from sebalaxi/office-translator-web-app:latest
+    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator:latest -f Dockerfile-controller ../../ --cache-from sebalaxi/office-translator:latest --push
+    docker buildx build --platform linux/amd64,linux/arm64 -t sebalaxi/office-translator-web-app:latest -f Dockerfile-web-app ../../web-app/ --cache-from sebalaxi/office-translator-web-app:latest --push
 else
     echo "build only mac release" 
     docker build -t sebalaxi/office-translator:latest -f Dockerfile-controller ../../
